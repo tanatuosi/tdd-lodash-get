da01272f70077d066e26048ee46115fa855ed086
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2024-05-14 02:01:05 +0800

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,51 @@
-import { demo } from ".";
+import { get } from ".";
 
-it("测试用例", () => {
-  expect(demo(1, 1)).toBe(true);
+it("基础测试", () => {
+  const demo = { a: 1 };
+  expect(get(demo, "a")).toBe(1);
+});
+
+it("基础测试2", () => {
+  const demo = { a: 2 };
+  expect(get(demo, "a")).toBe(2);
+});
+
+it("基础测试3", () => {
+  const demo = { a: 2 };
+  expect(get(demo, "c")).toBe(undefined);
+});
+
+it("基础测试4", () => {
+  const demo = { a: { c: 3 } };
+  expect(get(demo, "a.c")).toBe(3);
+});
+
+it("基础测试5", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, "a[0].b.c")).toBe(3);
+});
+
+it("基础测试6", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, "a[0].e.d")).toBe(undefined);
+});
+
+it("default测试1", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, "a[0].e.d", "test1")).toBe("test1");
+});
+
+it("数组测试1", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, ["a", "0", "b", "c"], "test1")).toBe(3);
+});
+
+it("数组测试3", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, ["a", "0", "b", "c"], "test1")).toBe(3);
+});
+
+it("数组测试2", () => {
+  const demo = { a: [{ b: { c: 3 } }] };
+  expect(get(demo, "['aff', '0sdf', '//b', 'c']", "test1")).toBe("test1");
 });
