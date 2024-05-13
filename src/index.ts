929fcd25132132be961155c2ae28fa859578a1ae
tanatuosi <49973816+tanatuosi@users.noreply.github.com>
2024-05-14 02:00:54 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function demo(a: any, b: any): boolean {
-  return a === b;
+// object (Object): 要检索的对象。
+// path (Array|string): 要获取属性的路径。
+// [defaultValue] (*): 如果解析值是 undefined ，这值会被返回。
+
+export function get(object: any, path: string | string[], defaultValue?: any) {
+  // a[0].b -> a.0.b -> ['a', '0', 'b']
+  const replacePath = Array.isArray(path)
+    ? path
+    : path.replace(/\[(\d+)\]/g, ".$1").split(".");
+  // const replacePath =  pathArray.replace(/\[(\d+)\]/g, ".$1");
+  console.log(replacePath);
+  const result = replacePath.reduce((acc: any, key: string) => {
+    return acc && acc[key] !== undefined ? acc[key] : undefined;
+  }, object);
+  return result === undefined ? defaultValue : result;
 }
+
+console.log(get({ a: [{ b: { c: 3 } }] }, ["a", "0", "b", "c"], "test1"));
